63e9a3d96e0bf095177c31292233c3d32e0dc69d
nnikcho-5m <nikolaypgee2@gmail.com>
2025-12-29 16:22:30 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'nnikcho'
+description 'Fivem Loading Screen'
+version '1.0'
+
+loadscreen 'web/index.html'
+
+files {
+    "web/*",
+    "web/assets/*",
+}
